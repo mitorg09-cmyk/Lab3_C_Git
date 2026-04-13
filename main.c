b5ed3ff0759c98a6ba97c7e2352dc96703cabec2
Mitrofan <mityirg09@mail.ru>
2026-04-13 06:01:30 -0400

    Added base matrix functions:
    1) For Create matrix
    2) For Clear matrix
    3) For print matrix to console

--- main.c
+++ main.c
@@ -1,7 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int** CreateMatrix(size_t row, size_t col, int rangeA, int rangeB);
+void clearMatrix(int*** matrix, size_t row);
+int printMatrix(int** matrix, size_t row, size_t col);
 
 int main()
 {
-    printf("Hello World!\n");
+    srand(time(0));
+
+    int** matrix = CreateMatrix(9999999, 9999999, -10, 10);
+    //printMatrix(matrix, 1024, 1024);
+
     return 0;
+}
+
+int** CreateMatrix(size_t row, size_t col, int rangeA, int rangeB)
+{
+  if(rangeA > rangeB)
+  {
+    int tmp = rangeA;
+    rangeA = rangeB;
+    rangeB = tmp;
+  }
+
+  int** matrix = (int**)calloc(row, sizeof(int*));
+
+  if(matrix)
+  {
+    for(int i = 0; i < row; i++)
+    {
+      matrix[i] = (int*)calloc(col, sizeof(int));
+      if(!matrix[i])
+      {
+        clearMatrix(&matrix, i);
+        return  NULL;
+      }
+
+      for(int j = 0; j < col; j++)
+      {
+        matrix[i][j] = rand() % (rangeB - rangeA + 1) + rangeA;
+      }
+    }
+  }
+
+  return matrix;
+}
+
+void clearMatrix(int*** matrix, size_t row)
+{
+  if(matrix && *matrix)
+  {
+    for(int i = 0; i < row; i++)
+    {
+      free((*matrix)[i]);
+      (*matrix)[i] = NULL;
+    }
+    free(*matrix);
+    *matrix = NULL;
+    printf("Cleared"); // For debug
+  }
+}
+
+int printMatrix(int** matrix, size_t row, size_t col)
+{
+  if(matrix)
+  {
+    for(int i = 0; i < row; i++)
+    {
+      if(matrix[i])
+      {
+        for(int j = 0; j < col; j++)
+        {
+          printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+      }
+      else
+      {
+        printf("NULL \n");
+      }
+    }
+    return 0;
+  }
+  return 1;
 }
