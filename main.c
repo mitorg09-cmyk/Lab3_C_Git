bdf9cd99e9902e4e8661f6af2f49174b936ee753
Mitrofan <mityirg09@mail.ru>
2026-04-18 18:05:26 -0400

    Added transpose function
    Добавелна функция TranspsMx, которая выполняет транспонирование разреженной матрицы. Добавлены проверки указателей внутри функции. Функция создает матрицу нужного количества строк и столбцов опираяси на переданные аргументы через функцию CreateMatrix, далее происходит заполнение новой матрицы, очистка старой и перезапись указателя по переданному тройному указателю.

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <time.h>
 
 int** CreateMatrix(size_t row, size_t col, int rangeA, int rangeB);
-void clearMatrix(int*** matrix, size_t row);
-int printMatrix(int** matrix, size_t row, size_t col);
+void ClearMatrix(int*** matrix, size_t row);
+int PrintMatrix(int** matrix, size_t row, size_t col);
+int TranspsMx(int*** ptrMatrix, size_t row, size_t col);
 
 int main()
 {
     srand(time(0));
-    int** matrix = CreateMatrix(999, 999999, -10, 10);
-
+    int** matrix = CreateMatrix(10, 10, -10, 10);
+    PrintMatrix(matrix, 10, 10);
+    TranspsMx(&matrix, 10, 10);
+    PrintMatrix(matrix, 10, 10);
     return 0;
 }
 
@@ -26,49 +29,53 @@ int** CreateMatrix(size_t row, size_t col, int rangeA, int rangeB)
   int** matrix = (int**)calloc(row, sizeof(int*));
   if(matrix)
   {
-    for(int i = 0; i < row; i++)
+    for(int i = 0; (size_t)i < row; i++)
     {
       matrix[i] = (int*)calloc(col, sizeof(int));
       if(!matrix[i])
       {
-        clearMatrix(&matrix, i);
+        ClearMatrix(&matrix, i);
         return  NULL;
       }
 
-      for(int j = 0; j < col; j++)
+      for(int j = 0; (size_t)j < col; j++)
       {
         matrix[i][j] = rand() % (rangeB - rangeA + 1) + rangeA;
       }
     }
   }
+  else // For debug
+  {
+    printf("From row create");
+  }
 
   return matrix;
 }
 
-void clearMatrix(int*** matrix, size_t row)
+void ClearMatrix(int*** ptrMatrix, size_t row)
 {
-  if(matrix && *matrix)
+  if(ptrMatrix && *ptrMatrix)
   {
-    for(int i = 0; i < row; i++)
+    for(int i = 0; (size_t)i < row; i++)
     {
-      free((*matrix)[i]);
-      (*matrix)[i] = NULL;
+      free((*ptrMatrix)[i]);
+      (*ptrMatrix)[i] = NULL;
     }
-    free(*matrix);
-    *matrix = NULL;
-    printf("Cleared"); // For debug
+    free(*ptrMatrix);
+    *ptrMatrix = NULL;
+    printf("Cleared \n"); // For debug
   }
 }
 
-int printMatrix(int** matrix, size_t row, size_t col)
+int PrintMatrix(int** matrix, size_t row, size_t col)
 {
   if(matrix)
   {
-    for(int i = 0; i < row; i++)
+    for(int i = 0; (size_t)i < row; i++)
     {
       if(matrix[i])
       {
-        for(int j = 0; j < col; j++)
+        for(int j = 0; (size_t)j < col; j++)
         {
           printf("%d ", matrix[i][j]);
         }
@@ -81,5 +88,32 @@ int printMatrix(int** matrix, size_t row, size_t col)
     }
     return 0;
   }
-  return 1;
+  return -1;
+}
+
+int TranspsMx(int*** ptrMatrix, size_t row, size_t col)
+{
+  if(!ptrMatrix || !*ptrMatrix)
+  {
+    return -1;
+  }
+
+  int** trspsMx = CreateMatrix(col, row, 0, 0);
+  if(!trspsMx)
+  {
+    return 1;
+  }
+
+  for(int i = 0; (size_t)i < row; i++)
+  {
+    if(!(*ptrMatrix)[i]) for(int j = 0; (size_t)j < col; j++) trspsMx[j][i] = 0;
+    else
+    {
+      for(int j = 0; (size_t)j < col; j++) trspsMx[j][i] = (*ptrMatrix)[i][j];
+    }
+  }
+
+  ClearMatrix(ptrMatrix, row);
+  *ptrMatrix = trspsMx;
+  return 0;
 }
