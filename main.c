8d1236a9fc54b9d07346b7a1f878e0386c8e50ac
Mitrofan <mityirg09@mail.ru>
2026-04-18 20:16:00 -0400

    Added sum matrix function
    Добавлена функция сложени двух матриц. Выполняется сверка количества строк и столбцов матриц, проверяются указатели. Функция оптимизирована для работы с разреженными матрицами. В функции создается дополнительная матрица, указатель которой возвращается в качестве результата.

--- main.c
+++ main.c
@@ -6,14 +6,22 @@ int** CreateMatrix(size_t row, size_t col, int rangeA, int rangeB);
 void ClearMatrix(int*** matrix, size_t row);
 int PrintMatrix(int** matrix, size_t row, size_t col);
 int TranspsMx(int*** ptrMatrix, size_t row, size_t col);
+int** SumMx(int** mx1, int** mx2, size_t row1, size_t col1, size_t row2, size_t col2);
 
 int main()
 {
     srand(time(0));
+
     int** matrix = CreateMatrix(10, 10, -10, 10);
+    int** matrix2 = CreateMatrix(10, 10, -10, 10);
+
     PrintMatrix(matrix, 10, 10);
-    TranspsMx(&matrix, 10, 10);
-    PrintMatrix(matrix, 10, 10);
+    PrintMatrix(matrix2, 10, 10);
+
+    int** smMx = SumMx(matrix, matrix2, 10, 10, 10, 10);
+
+    PrintMatrix(smMx, 10, 10);
+
     return 0;
 }
 
@@ -86,6 +94,7 @@ int PrintMatrix(int** matrix, size_t row, size_t col)
         printf("NULL \n");
       }
     }
+    printf("\n");
     return 0;
   }
   return -1;
@@ -116,4 +125,37 @@ int TranspsMx(int*** ptrMatrix, size_t row, size_t col)
   ClearMatrix(ptrMatrix, row);
   *ptrMatrix = trspsMx;
   return 0;
+}
+
+int** SumMx(int** mx1, int** mx2, size_t row1, size_t col1, size_t row2, size_t col2)
+{
+  if(!mx1 || !mx2)
+  {
+    return NULL;
+  }
+  if(row1 != row2 || col1 != col2)
+  {
+    return NULL;
+  }
+
+  int** smMx = CreateMatrix(row1, col1, 0, 0);
+  if(!smMx)
+  {
+    return NULL;
+  }
+
+  for(int i = 0; (size_t)i < row1; i++)
+  {
+    if(mx1[i] && mx2[i])
+      for(int j = 0; (size_t)j < col1; j++)
+        smMx[i][j] = mx1[i][j] + mx2[i][j];
+    else if(mx1[i])
+      for(int j = 0; (size_t)j < col1; j++)
+        smMx[i][j] = mx1[i][j];
+    else if(mx2[i])
+      for(int j = 0; (size_t)j < col1; j++)
+        smMx[i][j] = mx2[i][j];
+  }
+
+  return smMx;
 }
